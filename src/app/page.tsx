7f1b30d792fe2df3a16d43bdda7d3d93b2fe7113
Nixon Mbeki <nwokila@gmail.com>
2024-07-29 01:11:26 +0300

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://res.cloudinary.com/dja66bd2c/image/upload/v1720116861/samples/woman-on-a-football-field.jpg",
@@ -12,12 +13,17 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48 p-4">
+          <div key={Math.random()} className="w-48 p-4">
             <img src={image.url} />
           </div>
         ))}
