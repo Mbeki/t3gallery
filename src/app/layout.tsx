dbc8d3ed00e61a2eb749055a1a19295e8e1b4406
Nixon Mbeki <nwokila@gmail.com>
2024-07-29 20:54:19 +0300

    set up shacd-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en" className={`${GeistSans.variable} `}>
+      <html lang="en" className={`${GeistSans.variable} dark`}>
         <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         <body className="grid h-screen grid-rows-[auto,1fr]">
           <div>
