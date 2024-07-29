f6104a1eeecabdccb565575675654cbd4994fdff
Nixon Mbeki <nwokila@gmail.com>
2024-07-29 22:50:54 +0300

    analytics on delete button

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -4,6 +4,7 @@ import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { images } from "./db/schema";
+import analyticsServerClient from "./analytics";
 
 export async function getMyImages() {
   const user = auth();
@@ -36,6 +37,13 @@ export async function deleteImage(id: number) {
   // await db
   //   .delete(images)
   //   .where(and(eq(images.id, id), eq(images.userId, user.userId)));
+  analyticsServerClient.capture({
+    distinctId: user.userId,
+    event: "delete image",
+    properties: {
+      imageId: id,
+    },
+  });
 
   // revalidatePath("/");
   redirect("/");
